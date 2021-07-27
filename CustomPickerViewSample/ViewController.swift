3104f589798e2406e89f774897356a5e7b12ed88
fummicc1 <fumiya.tennis1@gmail.com>
2021-07-27 19:14:07 +0900

    データの表示まで

--- CustomPickerViewSample/ViewController.swift
+++ CustomPickerViewSample/ViewController.swift
@@ -1,19 +1,69 @@
-//
-//  ViewController.swift
-//  CustomPickerViewSample
-//
-//  Created by Fumiya Tanaka on 2021/07/27.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet var foodPickerView: UIPickerView! // 食べ物のPickerView
+    @IBOutlet var drinkPickerView: UIPickerView! // 飲み物のPickerView
+    
+    // 飲み物のデータの配列
+    var drinkArray: [String] = [
+        "爽健美茶",
+        "麦茶",
+        "コーラ",
+        "オレンジジュース",
+        "天然水"
+    ]
+    
+    // 食べ物のデータの配列
+    var foodArray: [String] = [
+        "ラーメン",
+        "トマト",
+        "生ハム",
+        "きゅうり",
+        "納豆",
+        "お米"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // データソースの設定
+        drinkPickerView.dataSource = self
+        foodPickerView.dataSource = self
+        
+        // デリゲートの設定
+        drinkPickerView.delegate = self
+        foodPickerView.delegate = self
     }
 
+    // PickerView一つあたりのの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // PickerView一つあたりの行の数
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if pickerView == foodPickerView {
+            return foodArray.count
+        }
+        
+        if pickerView == drinkPickerView {
+            return drinkArray.count
+        }
+        
+        return 0
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == foodPickerView {
+            return foodArray[row]
+        }
+        if pickerView == drinkPickerView {
+            return drinkArray[row]
+        }
+        return nil
+    }
 
 }
 
